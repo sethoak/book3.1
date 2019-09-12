547611122fd894e00997b34f46bcb1c525f82f59
sethoak <sethoak@gmail.com>
2019-09-12 09:46:30 -0500

    updated exercise 2

---
+++ exercise/js/main2.js
@@ -0,0 +1,13 @@
+import library from "./library.js";
+/*
+    Purpose:
+    This code is where a member, Rose, checks out a book.
+*/
+
+import libraryMembers from "./libraryMembers.js";
+
+let booksRose = anotherFile.getCheckedOutBooks("Rose", "Thorne");
+console.log("Rose's Books: ", booksRose);
+
+libraryMembers.memberCheckOutBook("Rose", "Thorne", "The Little Prince");
+console.log("Rose's Books: ", booksRose);
